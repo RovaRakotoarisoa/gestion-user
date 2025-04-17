2441596f6ca7ec29ba464ed5a449561d574fc66c
RovaRakotoarisoa <rovarakotoarisoa114@gmail.com>
2025-04-17 15:34:31 +0300

    make /dashboard route to home

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
+Route::redirect('/dashboard', '/home');
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
