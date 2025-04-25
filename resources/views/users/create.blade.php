787fcb067509b669a51ab6e005f96531a4fc9963
RovaRakotoarisoa <rovarakotoarisoa114@gmail.com>
2025-04-25 22:49:38 +0300

    design radio to Admin and user in create user

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -18,19 +18,33 @@
 				</div>
 			</div>
 			<div class="flex gap-4 mt-6">
-				<div>
-					{{-- <x-input name="role" type="radio" value="Admin" class="hidden peer/admin"/> --}}
-					<input type="radio" name="role" value="Admin" class="hidden">
-					<label for="admin" class="px-6 py-2 border border-gray-300 rounded-lg cursor-pointer text-gray-700 transition">
-						Admin
-					</label>
-				</div>
-				<div>
-					<input name="role" type="radio" value="User" class="hidden"/>
-					<label for="user" class="px-6 py-2 border border-gray-300 rounded-lg cursor-pointer text-gray-700">
-						User
-					</label>
-				</div>
+				<label for="admin" class="relative flex flex-col bg-white px-12 pt-4 rounded-lg shadow-md cursor-pointer">
+		          <span class="font-semibold text-gray-500 leading-tight uppercase">Admin</span>
+		          <input type="radio" name="role" id="admin" value="Admin" class="absolute h-0 w-0 appearance-none hidden" />
+
+		          <span aria-hidden="true" class="hidden absolute inset-0 border-2 border-green-500 bg-green-200 bg-opacity-10 rounded-lg">
+		            <span class="absolute top-1 right-1 h-6 w-6 inline-flex items-center justify-center rounded-full bg-green-200">
+		              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5 text-green-600">
+		                <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+		              </svg>
+		            </span>
+		          </span>
+			    </label>
+
+			    <label for="user" class="relative flex flex-col bg-white px-12 py-4 rounded-lg shadow-md cursor-pointer">
+		          <span class="font-semibold text-gray-500 leading-tight uppercase">User</span>
+		          <input type="radio" name="role" id="user" value="User" class="absolute h-0 w-0 appearance-none hidden" />
+
+		          <span aria-hidden="true" class="hidden absolute inset-0 border-2 border-green-500 bg-green-200 bg-opacity-10 rounded-lg">
+		            <span class="absolute top-1 right-1 h-6 w-6 inline-flex items-center justify-center rounded-full bg-green-200">
+		              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5 text-green-600">
+		                <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+		              </svg>
+		            </span>
+		          </span>
+			    </label>
+
+				
 			</div>
 			<div>
 				<x-input name="avatar" type="file" />
@@ -42,3 +56,4 @@
 		</form>
 	</x-container>
 </x-layout>
+
