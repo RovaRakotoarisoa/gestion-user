3b4c0cda8e548633452d254c7c9fbe0f4ed821d4
RovaRakotoarisoa <rovarakotoarisoa114@gmail.com>
2025-05-24 22:04:46 +0300

    make a scopeSearch for Search Don't forget scope into search on this models but it's Search on he models livewire

--- app/Models/User.php
+++ app/Models/User.php
@@ -68,4 +68,7 @@ class User extends Authenticatable
         ];
     }
 
+    public function scopeSearch($query, $value){
+        $query->where('name','like', "%{$value}%")->orWhere('email','like', "%{$value}%");
+    }
 }
