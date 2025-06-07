1c173053e797b6e2c0b050d305bf1a7899224899
RovaRakotoarisoa <rovarakotoarisoa114@gmail.com>
2025-06-07 14:59:24 +0300

    populate database with factory and call this factory to seedeers

--- resources/views/livewire/user-table.blade.php
+++ resources/views/livewire/user-table.blade.php
@@ -1,8 +1,8 @@
 <div id="user-table" class="space-y-4">
     <div
-        x-data="{open: false}" x-cloak 
-        class="bg-white p-6 rounded-lg flex-col space-y-10"
-    >
+         x-data="{open: false}" x-cloak 
+         class="bg-white p-6 rounded-lg flex-col space-y-10"
+        >
         <div class="flex justify-between items-center">
             <div class="flex justify-between items-center">
                 <input
@@ -18,7 +18,7 @@
                         x-on:click ="open = !open" 
                         class="flex justify-center font-bold !text-gray-600 !bg-[#f9fafb] !border !border-gray-300 !py-[10px] inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150"
                         type="button"
-                    >
+                        >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" class="w-4 h-4 gap-1">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"/>
                     </svg>
@@ -132,7 +132,7 @@
                         <td class="py-3 flex items-center gap-4">
                             <span class="drop-shadow-xl">
                                 @if ($user->profile_photo_path)
-                                    <img src="{{ asset('storage/' . $user->profile_photo_path) }}" alt="Avatar" class="w-10 h-10 rounded-full">
+                                    <img src="{{ asset('storage/' . $user->profile_photo_path) }}" alt="Avatar" class="w-10 h-10 rounded-full object-cover">
                                 @else
                                     <svg width="2.5rem" height="2.5rem" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" clip-rule="evenodd" d="M18 10C18 14.4183 14.4183 18 10 18C5.58172 18 2 14.4183 2 10C2 5.58172 5.58172 2 10 2C14.4183 2 18 5.58172 18 10ZM12.5 7.5C12.5 8.88071 11.3807 10 10 10C8.61929 10 7.5 8.88071 7.5 7.5C7.5 6.11929 8.61929 5 10 5C11.3807 5 12.5 6.11929 12.5 7.5ZM10 12C8.04133 12 6.30187 12.9385 5.20679 14.3904C6.39509 15.687 8.1026 16.5 10 16.5C11.8974 16.5 13.6049 15.687 14.7932 14.3904C13.6981 12.9385 11.9587 12 10 12Z" fill="#252626"/>
@@ -143,7 +143,7 @@
                                 {{ $user->name }}
                             </span>
                         </td>
-                        <td class="px-4 py-3 text-sm text-gray-600 truncate" title="{{ $user->email }}">
+                        <td class="py-3 text-sm text-gray-600 truncate" title="{{ $user->email }}">
                             {{ $user->email }}
                         </td>
                         <td class="py-3 text-sm text-gray-500 capitalize">
@@ -160,6 +160,12 @@
                                     Edit
                                 </a>
 
+                                {{--
+                                    PROBLEME TO SOLVE
+                                    Probleme with route 
+                                    It delete when route is only localhost/users 
+                                    but not where route is localhost/users?page=2 
+                                --}}
                                 <form action="{{ route('users.destroy', $user->id) }}" method="POST" class="inline">
                                     @csrf
                                     @method('DELETE')
