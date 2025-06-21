c8d1262c208dd7e24967ae7b418d88a60e8ad910
RovaRakotoarisoa <rovarakotoarisoa114@gmail.com>
2025-06-21 14:13:55 +0300

    add icon to search

--- resources/views/livewire/user-table.blade.php
+++ resources/views/livewire/user-table.blade.php
@@ -4,13 +4,16 @@
          class="bg-white p-6 rounded-lg flex-col space-y-10"
         >
         <div class="flex justify-between items-center">
-            <div class="flex justify-between items-center">
+            <div class="flex flex-row-reverse justify-between items-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg !w-[345px]">
                 <input
                     wire:model.live.debounce.250ms="search"
                     type="search" 
                     name="search" 
                     placeholder="Search User..."
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-100">
+                    class="bg-gray-50 border-none text-gray-900 text-sm rounded-lg focus:ring-transparent !w-[345px]">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" class="w-4 h-4 ml-3">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"/>
+                    </svg>
             </div>
             <div class="flex justify-between items-center gap-4">
                 <button
@@ -38,7 +41,7 @@
                 </div>
             </div>
         </div>
-        <template  x-if="open" x-transition>
+        <template x-if="open" x-transition>
                 <div
                 
                 class="flex items-center gap-3">
@@ -65,9 +68,10 @@
                         <option value="Admin">Admin</option>
                     </select>
                 </div>
-                <div class="flex items-center gap-1">
+                {{-- DON'T FORGET TO IMPLEMENT THIS --}}
+                {{-- <div class="flex items-center gap-1">
                     <input type="date" name="fromDate" id="fromDate" wire:model.live="fromDate" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg">
-                </div>
+                </div> --}}
             </div>
         </template>
         
@@ -133,7 +137,7 @@
                                     x-on:click ="is_bulkAction = !is_bulkAction"
                                     type="checkbox" 
                                     name="user_ids[]" 
-                                    value="{{ $user->id }}" 
+                                    value="{{ $user->id }}"
                                     class="rounded border border-blue-600/20 text-purple-300 shadow-lg"
                             >
                         </td>
